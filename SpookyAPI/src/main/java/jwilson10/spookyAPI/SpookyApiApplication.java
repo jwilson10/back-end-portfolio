6bf2ef081e79b1c7f71592ed4c91656c7e6f811f
jwilson10 <jessica.a.wilson99@gmail.com>
2023-01-13 10:33:45 -0500

    Added Java Spring application with dependencies

---
+++ SpookyAPI/src/main/java/jwilson10/spookyAPI/SpookyApiApplication.java
@@ -0,0 +1,13 @@
+package jwilson10.spookyAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpookyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpookyApiApplication.class, args);
+	}
+
+}
---
+++ SpookyAPI/src/test/java/jwilson10/spookyAPI/SpookyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package jwilson10.spookyAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpookyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
